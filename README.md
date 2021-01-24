dd08fb909436fe6397ad3aa1da39b0561c6336e1
Isabell Danell <imdanell@gmail.com>
2021-01-24 15:34:11 -0600

    deploying

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # UserDirectory-19
+Unit 19 React Homework: Employee Directory
+
+## User Story
+
+***README from BCS*** As a user, I want to be able to view my entire employee directory at once so that I have quick access to their information.
---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import './App.css';
+import {useEffect, useState} from 'react';
+
+function App() {
+
+const [employees, setEmployees] = useState([]);
+const [inputs, setInputs] = useState({});
+const [filteredEmployees, setFilteredEmployees] = useState([]);
+
+useEffect(() => {
+  fetch('https://randomuser.me/api/?results=50&inc=picture,name,phone,email,dob')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.results)
+    setEmployees(data.results)
+    setFilteredEmployees(data.results)
+  })
+}, [])
+
+const handleInputs = (event) => {
+  const {name,value} = event.target;
+  var filteredEmployees = (employees.filter(
+  (each => each.name.first.includes(value) || 
+  (each.name.last.includes(value)) || 
+  (each.phone.includes(value)) || 
+  (each.email.includes(value)) ||
+  (each.dob.date.includes(value)))))
+console.log(filteredEmployees);
+setFilteredEmployees(filteredEmployees);
+
+}
+
+  return (
+    <>
+    <header>
+    <h1>Employee Directory</h1>
+    <h5>Sort employees by using the search box to narrow your results</h5>
+    </header>
+
+    <input name="searchTerm" onChange={handleInputs} placeholder="Search"></input>
+    <div className="employees-grid">
+    <div>
+        Image
+          </div>
+          <div>
+          Name 
+          </div>
+          <div>
+         Phone
+          </div>
+          <div>
+         Email
+          </div>
+          <div>
+          DOB
+          </div>
+
+    </div>
+    {filteredEmployees.map((each,index) => {
+      return(
+        
+        <div key={index} className="employees-grid">
+          <div>
+          <img src={each.picture.thumbnail} alt={index}/>
+          </div>
+          <div>
+          {each.name.first} {each.name.last}
+          </div>
+          <div>
+          {each.phone}
+          </div>
+          <div>
+          {each.email}
+          </div>
+          <div>
+          {each.dob.date}
+          </div>
+        </div>
+      )
+    })
+    }
+    </>
+  );
+}
+
+export default App;
